dc80394b503863af1b6d2cdb72b4b2ba5afab1dc
Diana Rojo <licdianarojo@gmail.com>
2025-11-17 21:03:56 -0600

    3.7. Integración de base de datos  Diana Rojo Se integró a la aplicación la base de datos base-de-datos-diana-flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,26 @@
 import 'package:flutter/material.dart';
-import 'screens/pokemon_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Pokemon App',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: const PokemonScreen(),
+      title: 'Firebase Test',
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Conexión Firebase')),
+        body: const Center(child: Text('Firebase Iniciado')),
+      ),
     );
   }
 }
